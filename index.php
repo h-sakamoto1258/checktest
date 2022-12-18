88155743fcd898cada9401b73bd04eb6a5facde9
h-sakamoto1258 <114583749+h-sakamoto1258@users.noreply.github.com>
2022-12-19 05:10:59 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -46,9 +46,12 @@ $post_data['comment'];
         <tbody>
             <?php
             try {
-                $stt = $db->prepare('SELECT = FROM posts ORDER BY published DESC');
+                $stt = $db->prepare('SELECT = FROM posts ORDER BY id DESC');
                 $stt->execute();
                 while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+            <?php
+                //foreach($stt as $$row) {
             ?>
             <tr>
                 <td><?=e($post_data['id']) ?></td>
@@ -72,80 +75,3 @@ $post_data['comment'];
             </body>
             </html>
 
-<?php
-/*
-    <div class="main"></div>
-    <table border="1" align="center">
-    <tr bgcolor="#CCFFFF">
-        <th width="auto">記事ID</th>
-        <th width="auto">タイトル</th>
-        <th width="auto">カテゴリ</th>
-        <th width="auto">本文</th>
-        <th width="auto">投稿日</th>
-    </tr>
-    <?php foreach ($post_date as $key => $value) : { ?>
-        <tr>
-            <td>
-                <?=$key ?>
-            </td>
-        </tr>
-    <?php } ?>
-    </table>
-            <?php /*foreach ($post_date as $post_data) ; 
-            <td><?= $post_data['id']?></td>
-            <td><?= $post_data['title']?></td>
-            <td><?= $post_data['created']?></td>
-            <td><?= $post_data['category_no']?></td>
-            <td><?= $post_data['comment']?></td>
-            
-            ?>
-            <?php
-            foreach ($post_date as $key => $value) {
-                echo $key. ':'. $value.'<br/>';
-            }
-            ?>
-        </tr>
-        <?php //endforeach ?>
-    </table>
-    <?php
-    /*
-        $sql = "SELECT * FROM posts";
-        $pdo = db_connect();
-        try {
-            $stml = $pdo->prepare($sql);
-            //$stml->bindParam(':id',$id);
-            $stml->execute();
-            while ($row = $stml->fetch(PDO::FETCH_ASSOC)) {
-                //$row['id']=$id . $row['title']=$title . $row['category_no']=$category_no . $row['comment']=$comment . $row['created']=$crested;
-                echo $row['id'] . ',' . $row['title'] . ',' . $row['category_no'] . ',' . $row['comment'] . ',' . $row['created'];
-                echo '<br />';
-            }
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-            die();
-        }
-        /*$db = getUserData();
-        $db->exec();
-        $db = mysql_query("SELECT * FROM users");
-        print "<html>";
-        print "<body>";
-        print "<table border = 1 >";
-        print "<tr><td>記事ID</td><td>タイトル</td><td>カテゴリ</td><td>本文</td><td>投稿日</td></tr>";
-        while($que = mysql_fetch_array($db)) {
-            print "<tr><td>" , $que["id"] , "</td>";
-            print "<td>" , $que["first_name"] , "</td>";
-            print "<td>" , $que["last_name"] , "</td>";
-            print "<td>" , $que["last_login"] , "</td></tr>";
-        print $db->getAttribute(PDO:ATTR_SERVER_INFO);
-        }
-        print "</table>";
-        print "</body>";
-        print "</html>";
-
-    <body>
-        <table border="1">
-            <tr><td>記事ID</td><td>タイトル</td><td>カテゴリ</td><td>本文</td><td>投稿日</td></tr>
-            <td>$id</td><td>$title</td><td>$category_no</td><td>$comment</td><td>$created</td>
-        </table>
-        */
-?>
